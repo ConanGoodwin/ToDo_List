c99a327d0741e8b6994bd9322f1c77ba023c904f
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-26 19:50:00 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+function destacaTarefa(evento) {
+  evento.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 function addTarefa() {
   const txtTarefa = document.getElementById('texto-tarefa');
   const listaTarefa = document.getElementById('lista-tarefas');
   const tagLi = document.createElement('li');
 
+  tagLi.addEventListener('click', destacaTarefa);
   tagLi.innerText = txtTarefa.value;
-  txtTarefa.value = "";
+  txtTarefa.value = '';
 
   listaTarefa.appendChild(tagLi);
 }
 
 document.getElementById('criar-tarefa').addEventListener('click',addTarefa);
+// rgb(128, 128, 128)
